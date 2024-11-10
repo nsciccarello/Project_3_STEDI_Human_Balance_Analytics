ae2118202020564f94cb04dedc7bd4c42f12570c
Nicole Watson Ciccarello <94207359+nsciccarello@users.noreply.github.com>
2024-11-10 17:24:33 -0500

    Add files via upload

---
+++ sql_scripts/accelerometer_landing.sql
@@ -0,0 +1,17 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS `stedi_project_database`.`accelerometer_landing` (
+  `user` string,
+  `timeStamp` bigint,
+  `x` float,
+  `y` float,
+  `z` float
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+WITH SERDEPROPERTIES (
+  'ignore.malformed.json' = 'FALSE',
+  'dots.in.keys' = 'FALSE',
+  'case.insensitive' = 'TRUE',
+  'mapping' = 'TRUE'
+)
+STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION 's3://stedi-human-balance-analytics-project/landing_zone/accelerometer_landing/'
+TBLPROPERTIES ('classification' = 'json');
---
+++ sql_scripts/customer_landing.sql
@@ -0,0 +1,22 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS `stedi_project_database`.`customer_landing` (
+  `serialnumber` string,
+  `sharewithpublicasofdate` bigint,
+  `birthday` string,
+  `registrationdate` bigint,
+  `sharewithresearchasofdate` bigint,
+  `customername` string,
+  `email` string,
+  `lastupdatedate` bigint,
+  `phone` string,
+  `sharewithfriendsasofdate` bigint
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+WITH SERDEPROPERTIES (
+  'ignore.malformed.json' = 'FALSE',
+  'dots.in.keys' = 'FALSE',
+  'case.insensitive' = 'TRUE',
+  'mapping' = 'TRUE'
+)
+STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION 's3://stedi-human-balance-analytics-project/landing_zone/customer_landing/'
+TBLPROPERTIES ('classification' = 'json');
